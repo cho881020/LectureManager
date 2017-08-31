e0b24c7ddc22b84d9a789148ce86ec55aef89b70
WhanDuS2 <cjh19890212@gmail.com>
2017-08-31 10:16:41 +0900

    키 해쉬 수집 이후

--- app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
@@ -1,10 +1,18 @@
 package kr.co.tjeit.lecturemanager;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,10 +21,26 @@ public class LoginActivity extends AppCompatActivity {
 
     public static LoginActivity myActivity;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "kr.co.tjeit.lecturemanager",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
         myActivity = this;
 
         signUpBtn = (Button) findViewById(R.id.signUpBtn);
