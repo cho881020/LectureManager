ceef2b61d566f59eb63ddf937adf2c7bc69dfef2
Iksang <thsdlrtkd@gmail.com>
2017-08-31 11:14:38 +0900

    User 데이터, ContextUtil 완료

---
+++ app/src/main/java/kr/co/tjeit/lecturemanager/data/User.java
@@ -0,0 +1,44 @@
+package kr.co.tjeit.lecturemanager.data;
+
+/**
+ * Created by tjoeun on 2017-08-31.
+ */
+
+public class User {
+    private String id;
+    private String name;
+    private String profileImgPath;
+
+    public User() {
+    }
+
+    public User(String id, String name, String profileImgPath) {
+        this.id = id;
+        this.name = name;
+        this.profileImgPath = profileImgPath;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfileImgPath() {
+        return profileImgPath;
+    }
+
+    public void setProfileImgPath(String profileImgPath) {
+        this.profileImgPath = profileImgPath;
+    }
+}
---
+++ app/src/main/java/kr/co/tjeit/lecturemanager/util/ContextUtil.java
@@ -0,0 +1,51 @@
+package kr.co.tjeit.lecturemanager.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.facebook.login.LoginManager;
+
+import kr.co.tjeit.lecturemanager.data.User;
+
+/**
+ * Created by tjoeun on 2017-08-31.
+ */
+
+public class ContextUtil {
+
+    private static User loginUser = null;
+    private static final String prefName = "LectureManagerPref";
+    private static final String USER_ID = "USER_ID";
+    private static final String USER_NAME = "USER_NAME";
+    private static final String USER_PROFILEIMG_PATH = "USER_PROFILEIMG_PATH";
+
+    public static void login(Context context, String id, String name, String imgPath){
+        SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
+        pref.edit().putString(USER_ID,id).apply();
+        pref.edit().putString(USER_NAME,name).apply();
+        pref.edit().putString(USER_PROFILEIMG_PATH,imgPath).apply();
+    }
+
+    public static void logout(Context context){
+        SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
+        pref.edit().putString(USER_ID,"").apply();
+        pref.edit().putString(USER_NAME,"").apply();
+        pref.edit().putString(USER_PROFILEIMG_PATH,"").apply();
+    }
+
+    public static User getLoginUserData(Context context){
+        SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
+        if(pref.getString(USER_ID,"").equals("")){
+            loginUser=null;
+        }
+        else{
+            loginUser = new User();
+            loginUser.setId(pref.getString(USER_ID,""));
+            loginUser.setName(pref.getString(USER_NAME,""));
+            loginUser.setProfileImgPath(pref.getString(USER_PROFILEIMG_PATH,""));
+        }
+        return loginUser;
+
+    }
+
+}
