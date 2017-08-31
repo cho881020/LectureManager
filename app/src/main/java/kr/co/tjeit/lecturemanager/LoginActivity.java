dd669a739b60caeb43a6d8cb48e1de9731031e54
zio4272 <zio4272@naver.com>
2017-08-31 11:02:26 +0900

    페이스북 로그인 구현

--- app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
@@ -2,11 +2,17 @@ package kr.co.tjeit.lecturemanager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
 import com.kakao.network.ErrorResult;
@@ -19,12 +25,14 @@ import com.kakao.util.exception.KakaoException;
 public class LoginActivity extends BaseActivity {
 
     KaKaoSessionCallback ksc;
+    CallbackManager cm;
 
     private Button signUpBtn;
     private Button loginBtn;
 
     public static LoginActivity myActivity;
     private com.kakao.usermgmt.LoginButton kakaoLoginBtn;
+    private com.facebook.login.widget.LoginButton fbLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +43,19 @@ public class LoginActivity extends BaseActivity {
         setupEvents();
         setValues();
 
+//        재접속 할때마다 페이스북 로그아웃
+        LoginManager.getInstance().logOut();
+//        재접속 할때마다 카카오톡 로그아웅
+        UserManagement.requestLogout(null);
+
 
     }
 
 
     @Override
     public void setupEvents() {
+
+
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,19 +78,53 @@ public class LoginActivity extends BaseActivity {
     @Override
     public void setValues() {
 
+
+        cm = new CallbackManager.Factory().create();
         ksc = new KaKaoSessionCallback();
         Session.getCurrentSession().addCallback(ksc);
         Session.getCurrentSession().checkAndImplicitOpen();
 
-        this.kakaoLoginBtn = (LoginButton) findViewById(R.id.kakaoLoginBtn);
+        ProfileTracker pt = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                if (currentProfile != null) {
+                    Toast.makeText(mContext, currentProfile.getName() + "로그인", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        };
+
+        fbLoginBtn.registerCallback(cm, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+
+
 
     }
 
     @Override
     public void bindViews() {
-        signUpBtn = (Button) findViewById(R.id.signUpBtn);
-        loginBtn = (Button) findViewById(R.id.loginBtn);
-
+        this.signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        this.kakaoLoginBtn = (LoginButton) findViewById(R.id.kakaoLoginBtn);
+        this.fbLoginBtn = (com.facebook.login.widget.LoginButton) findViewById(R.id.fbLoginBtn);
+        this.loginBtn = (Button) findViewById(R.id.loginBtn);
     }
 
     @Override
@@ -84,6 +133,7 @@ public class LoginActivity extends BaseActivity {
 
         }
         super.onActivityResult(requestCode, resultCode, data);
+        cm.onActivityResult(requestCode, resultCode, data);
     }
 
     public class KaKaoSessionCallback implements ISessionCallback {
