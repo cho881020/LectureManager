9bb90af5de1849268b80603a35c11f29dc4b9a83
unripeblue <syp06@naver.com>
2017-09-01 09:38:36 +0900

    출석체크 화면까지 그림

---
+++ app/src/main/java/kr/co/tjeit/lecturemanager/DailyCheckActivity.java
@@ -0,0 +1,28 @@
+package kr.co.tjeit.lecturemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DailyCheckActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_check);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
