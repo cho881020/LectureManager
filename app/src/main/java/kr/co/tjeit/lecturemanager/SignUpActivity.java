a34d62129fc9d1baf9c21f818d77aefcc16ae44e
zio4272 <zio4272@naver.com>
2017-09-04 11:04:03 +0900

    서버에 아이디가 중복인지 질문함.

--- app/src/main/java/kr/co/tjeit/lecturemanager/SignUpActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/SignUpActivity.java
@@ -1,26 +1,81 @@
 package kr.co.tjeit.lecturemanager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import kr.co.tjeit.lecturemanager.util.ContextUtil;
+import kr.co.tjeit.lecturemanager.util.GlobalData;
+import kr.co.tjeit.lecturemanager.util.ServerUtil;
 
 public class SignUpActivity extends BaseActivity {
 
     private Button signUpBtn;
+    private android.widget.EditText idEdt;
+    private Button checkDuplBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        bindViews();
+        setupEvents();
+        setValues();
 
 
     }
 
 
-
     @Override
     public void setupEvents() {
+
+        checkDuplBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                중복여부? => 서버에다가 물어보면 돌아오는 응답으로 체크.
+
+//                버튼이 눌리면 실제로 서버에 중복 확인 요청
+                ServerUtil.check_dupl_id(mContext, idEdt.getText().toString(), new ServerUtil.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+//                        응답에 포함된 json을 분석
+//                        json 내부에서 중복 여부를 파악.
+//                        파악된 중복 여부에 따라 중복확인 후처리 작업 진행.
+
+//                        json 파싱해서 isIdDupl을 상황에 맞는 값으로 세팅.
+                        boolean isIdDupl = true;
+
+                        if (isIdDupl) {
+//                    아이디가 중복된 상황.
+//                    아이디가 중복되었다면,
+//                    중복 확인 (제목), 이미 사용중인 아이디 입니다. (메세지)
+//                    확인 버튼만 있는 경고창 띄워주기.
+                            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                            builder.setTitle("중복 확인");
+                            builder.setMessage("이미 사용중인 아이디 입니다.");
+                            builder.setPositiveButton("확인", null);
+                            builder.show();
+
+                        } else {
+//                    아이디가 중복되지 않는 상황.
+                            Toast.makeText(mContext, "사용해도 좋은 아이디 입니다.", Toast.LENGTH_SHORT).show();
+//                    아이디가 중복되지 않으면, 사용해도 좋은 아이디 입니다. Toast
+                        }
+
+
+                    }
+                });
+
+
+            }
+        });
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,9 +93,12 @@ public class SignUpActivity extends BaseActivity {
     public void setValues() {
 
     }
+
     @Override
     public void bindViews() {
-        signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        this.signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        this.checkDuplBtn = (Button) findViewById(R.id.checkDuplBtn);
+        this.idEdt = (EditText) findViewById(R.id.idEdt);
 
     }
 }
--- app/src/main/java/kr/co/tjeit/lecturemanager/util/ServerUtil.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/util/ServerUtil.java
@@ -12,7 +12,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-
 /**
  * Created by KJ_Studio on 2015-12-24.
  */
@@ -20,7 +19,7 @@ public class ServerUtil {
 
     private static final String TAG = ServerUtil.class.getSimpleName();
 
-    private final static String BASE_URL = "http://tdd.team/"; // 라이브서버
+    private final static String BASE_URL = "http://13.124.238.13/"; // 라이브서버
 //    private final static String BASE_URL = "http://share-tdd.com/"; // 개발서버
 
     public interface JsonResponseHandler {
@@ -30,17 +29,15 @@ public class ServerUtil {
 
     // 사용자 관련 함수 모음
 
-    // 회원 가입
-    public static void facebook_login(final Context context, final String name, final String uid, final String email, final JsonResponseHandler handler) {
-        String url = BASE_URL+"mobile/facebook_login";
+    // 회원 가입시 아이디 중복 체크
+    public static void check_dupl_id(final Context context, final String id, final JsonResponseHandler handler) {
+        String url = BASE_URL + "mobile/check_dupl_id";
         //		String registrationId = ContextUtil.getRegistrationId(context);
 
         Map<String, String> data = new HashMap<String, String>();
-        data.put("uid", uid);
-        data.put("name", name);
-        data.put("email", email);
+        data.put("user_id", id);
 
-        AsyncHttpRequest.post(context, url,  data, true, new AsyncHttpRequest.HttpResponseHandler() {
+        AsyncHttpRequest.post(context, url, data, true, new AsyncHttpRequest.HttpResponseHandler() {
 
             @Override
             public boolean onPrepare() {
@@ -59,6 +56,51 @@ public class ServerUtil {
                     e.printStackTrace();
                 }
             }
+
+            @Override
+            public void onFinish() {
+
+            }
+
+            @Override
+            public void onCancelled() {
+
+            }
+
+        });
+    }
+
+
+    // 회원 가입
+    public static void facebook_login(final Context context, final String name, final String uid, final String email, final JsonResponseHandler handler) {
+        String url = BASE_URL + "mobile/facebook_login";
+        //		String registrationId = ContextUtil.getRegistrationId(context);
+
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("uid", uid);
+        data.put("name", name);
+        data.put("email", email);
+
+        AsyncHttpRequest.post(context, url, data, true, new AsyncHttpRequest.HttpResponseHandler() {
+
+            @Override
+            public boolean onPrepare() {
+                return true;
+            }
+
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response);
+                try {
+                    JSONObject json = new JSONObject(response);
+
+                    if (handler != null)
+                        handler.onResponse(json);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
             @Override
             public void onFinish() {
 
