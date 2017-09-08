b55001bc2540703f32e0d925d3ff92bd6401cec8
Iksang <thsdlrtkd@gmail.com>
2017-09-08 11:34:50 +0900

    푸시 기능 추가

--- app/src/main/java/kr/co/tjeit/lecturemanager/SplashActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/SplashActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,6 +15,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Toast.makeText(this, token, Toast.LENGTH_SHORT).show();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
